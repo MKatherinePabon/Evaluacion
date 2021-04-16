2213588089e47435fb6ad3e6ddf28dfeac949fe1
MKatherinePabon <79108149+MKatherinePabon@users.noreply.github.com>
2021-04-16 00:01:22 -0400

    Add files via upload

---
+++ respuesta2.pas
@@ -0,0 +1,15 @@
+program respuesta2;
+uses crt;
+begin
+  
+ writeln('¿Qué retorna la función EOF en pascal?');
+ writeln();
+ writeln(' En pascal, la función EOF, es una función booleana que retorna "True" en el momento en el que se llega al final del fichero.');
+writeln('En concreto, EOF es un booleano de fin de archivo. Cabe mencionar que no se puede confundir con EOLN que es un booleano de fin de línea');
+writeln();
+
+//Respuesta: En pascal, la función EOF, es una función booleana que retorna "True" en el momento en el que se llega al final del fichero.
+
+//En concreto, EOF es un booleano de fin de archivo. Cabe mencionar que no se puede confundir con EOLN que es un booleano de fin de línea.//
+  readln;
+end.
